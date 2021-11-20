cbb99c4ca6fdb2920646ea6c0925d1bb9446abac
adham618 <adhamtarek291gmail.com>
2021-11-20 15:33:44 +0200

    add route blogs/:id

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Create"
+import BlogDetails from './BlogDetails';
 function App() {
   return (
     <Router>
@@ -10,7 +11,8 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/create" element={<Create />}></Route>
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
           </Routes>
         </div>
       </div>
