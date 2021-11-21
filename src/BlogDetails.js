65ba51487c9756e9e177ce3815d417cd844adb42
adham618 <adhamtarek291gmail.com>
2021-11-21 23:17:14 +0200

    add NotFound.js page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,18 +1,27 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs/" + id);
+  const navigate = useNavigate();
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blogs.id, {
+      method: "DELETE"
+    }).then(() => {
+      navigate("/");
+    })
+  }
   return (
     <div className="blog-details">
       {error && <div>{error}</div>}
       {isPending && <div>Loading.....</div>}
       {blogs && (
         <article>
-          <h2>{blogs.title} :-</h2>
+          <h2>{blogs.title} :</h2>
           <p>Written by {blogs.author}</p>
           <p>{blogs.body}</p>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
